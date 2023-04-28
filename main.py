3c0d7c1e11c9d83aa6aa0c17843cfc310348a259
trevor mccarson <tmccarson@ithaca.edu>
2023-04-28 12:41:21 -0400

    yas

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 # This is the best project ever.
+import json
+import requests
 
+
+def get_pokemon_data(pokemon_id_or_name):
+    url = f"https://pokeapi.co/api/v2/pokemon/{pokemon_id_or_name}/"
+    data = requests.get(url)
+    return data
+
+
+class Quiz:
+
+    def __init__(self, quiz_type, gen):
+        self.quiz_type = quiz_type
+        self.gen = gen
 
